925a856351f8cc1997ba823c8c44bb2dfce58921
huangfq <fqin.huang@samsung.com>
2019-02-12 13:03:52 +0800

    Add tab

---
+++ demo/Tizen.NUI.Samples/Samples/TabSample.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Controls;
+
+namespace Tizen.NUI.Samples
+{
+    public class TabSample : IExample
+    {
+        private View root;
+
+        private TextLabel[] createText = new TextLabel[2];
+
+        private Tab tab = null;
+        private Tab tab2 = null;
+
+        private int index = 0;
+        private int index2 = 0;
+
+        private static string[] buttonStyles = new string[]
+        {
+            "UtilityTabButton",
+            "FamilyTabButton",
+            "FoodTabButton",
+            "KitchenTabButton",
+        };
+        private static Color[] color = new Color[]
+        {
+            new Color(0.05f, 0.63f, 0.9f, 1),//#ff0ea1e6
+            new Color(0.14f, 0.77f, 0.28f, 1),//#ff24c447
+            new Color(0.75f, 0.46f, 0.06f, 1),//#ffec7510
+            new Color(0.59f, 0.38f, 0.85f, 1),//#ff9762d9
+        };
+        public void Activate()
+        {
+            Window window = Window.Instance;
+
+            root = new View()
+            {
+                Size2D = new Size2D(1920, 1080),
+            };
+            window.Add(root);
+
+            ///////////////////////////////////////////////Create by Property//////////////////////////////////////////////////////////
+            createText[0] = new TextLabel();
+            createText[0].Text = "Create Tab just by properties";
+            createText[0].Size2D = new Size2D(500, 100);
+            createText[0].Position2D = new Position2D(200, 100);
+            root.Add(createText[0]);
+
+            tab = new Tab();
+            tab.Size2D = new Size2D(500, 108);
+            tab.Position2D = new Position2D(100, 300);
+            tab.BackgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
+
+            root.Add(tab);
+
+
+            ///////////////////////////////////////////////Create by Attributes//////////////////////////////////////////////////////////
+            createText[1] = new TextLabel();
+            createText[1].Text = "Create Tab just by Attributes";
+            createText[1].Size2D = new Size2D(450, 100);
+            createText[1].Position2D = new Position2D(1000, 100);
+            createText[1].MultiLine = true;
+            root.Add(createText[1]);
+
+            TabAttributes attrs = new TabAttributes
+            {
+                IsNatureTextWidth = false,
+                PaddingLeft = new IntSelector { All = 56 },
+                PaddingRight = new IntSelector { All = 56 },
+                PaddingTop = new IntSelector { All = 1 },
+                PaddingBottom = new IntSelector { All = 0 },
+                UnderLineAttributes = new ViewAttributes
+                {
+                    Size2D = new Size2DSelector { All = new Size2D(1, 3) },
+                    PositionUsesPivotPoint = new BoolSelector { All = true },
+                    ParentOrigin = new PositionSelector { All = Tizen.NUI.ParentOrigin.BottomLeft },
+                    PivotPoint = new PositionSelector { All = Tizen.NUI.PivotPoint.BottomLeft },
+                    BackgroundColor = new ColorSelector { All = color[0] },
+                },
+                TextAttributes = new TextAttributes
+                {
+                    PointSize = new FloatSelector { All = 25 },
+                    TextColor = new ColorSelector
+                    {
+                        Normal = Color.Black,
+                        Selected = color[0],
+                    },
+                },                
+            };
+
+            tab2 = new Tab(attrs);
+            tab2.Size2D = new Size2D(500, 108);
+            tab2.Position2D = new Position2D(900, 300);
+            tab2.BackgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+            tab2.ItemChangedEvent += Tab2ItemChangedEvent;
+            root.Add(tab2);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Tab.TabItem item = new Tab.TabItem();
+                item.Text = "Tab " + i;
+                tab2.AddItem(item);
+            }
+            tab2.SelectedItemIndex = 0;
+
+        }
+
+        private void Tab2ItemChangedEvent(object sender, Tab.ItemChangeEventArgs e)
+        {
+            createText[1].Text = "Create Tab just by Attributes, Selected index from " + e.PreviousIndex + " to " + e.CurrentIndex;
+        }
+
+        public void Deactivate()
+        {
+            if (root != null)
+            {
+                root.Remove(tab2);
+                tab2.Dispose();
+                tab2 = null;
+                
+                root.Remove(createText[0]);
+                createText[0].Dispose();
+                createText[0] = null;
+                root.Remove(createText[1]);
+                createText[1].Dispose();
+                createText[1] = null;
+
+                Window.Instance.Remove(root);
+                root.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Tizen.NUI/src/Controls/Tab.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Controls
+{
+    public class Tab : Control
+    {
+        private List<TabItem> itemList = new List<TabItem>();
+        private int curIndex = 0;
+        private View underLine = null;
+        private EventHandler<ItemChangeEventArgs> itemChangeHander;
+        private TabAttributes tabAttributes = null;
+
+        public Tab() : base()
+        {
+            tabAttributes = attributes as TabAttributes;
+            if (tabAttributes == null)
+            {
+                throw new Exception("Tab attribute parse error.");
+            }
+            Initialize();
+        }
+        public Tab(string style) : base(style)
+        {
+            tabAttributes = attributes as TabAttributes;
+            if (tabAttributes == null)
+            {
+                throw new Exception("Tab attribute parse error.");
+            }
+            Initialize();
+        }
+        public Tab(TabAttributes attributes) : base()
+        {
+            this.attributes = tabAttributes = attributes.Clone() as TabAttributes;
+            Initialize();
+        }
+
+        public event EventHandler<ItemChangeEventArgs> ItemChangedEvent
+        {
+            add
+            {
+                itemChangeHander += value;
+            }
+            remove
+            {
+                itemChangeHander -= value;
+            }
+        }
+
+        public int SelectedItemIndex
+        {
+            get
+            {
+                return curIndex;
+            }
+            set
+            {
+                if (value < itemList.Count)
+                {
+                    UpdateSelectedItem(itemList[value]);
+                }
+            }
+        }
+
+        public void AddItem(TabItem item)
+        {
+            int h = 0;
+            int paddingTop = 0;
+            if(tabAttributes.UnderLineAttributes != null && tabAttributes.UnderLineAttributes.Size2D != null)
+            {
+                h = tabAttributes.UnderLineAttributes.Size2D.All.Height;
+            }
+            if(tabAttributes.PaddingTop != null)
+            {
+                paddingTop = tabAttributes.PaddingTop.All.Value;
+            }
+            item.Size2D.Height = Size2D.Height - h - paddingTop;
+            item.Position2D.Y = paddingTop;
+            ApplyAttributes(item.TextItem, tabAttributes.TextAttributes);
+            item.TouchEvent += ItemTouchEvent;
+            Add(item);
+            item.Index = itemList.Count;
+            itemList.Add(item);
+
+            UpdateItem();
+            itemList[curIndex].State = States.Selected;
+            itemList[curIndex].UpdateItemText(tabAttributes.TextAttributes);
+            
+            UpdateUnderLinePos();
+        }
+
+        protected override void Dispose(DisposeTypes type)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (type == DisposeTypes.Explicit)
+            {
+                if (underLine != null)
+                {
+                    Remove(underLine);
+                    underLine.Dispose();
+                    underLine = null;
+                }
+            }
+
+            base.Dispose(type);
+        }
+
+        protected override void OnUpdate(Attributes attributtes)
+        {
+            tabAttributes = attributes as TabAttributes;
+            if (tabAttributes == null)
+            {
+                return;
+            }
+
+            ApplyAttributes(this, tabAttributes);
+        }
+
+        protected override Attributes GetAttributes()
+        {
+            return new TabAttributes();
+        }
+
+        private void Initialize()
+        {
+            CreateUnderLine();
+        }
+
+        private void CreateUnderLine()
+        {
+            if (tabAttributes.UnderLineAttributes != null)
+            {
+                if (underLine == null)
+                {
+                    underLine = new View();
+                    Add(underLine);
+                }
+                ApplyAttributes(underLine, tabAttributes.UnderLineAttributes);
+                underLine.RaiseToTop();
+            }
+        }
+
+        private void UpdateItem()
+        {
+            int totalNum = itemList.Count;
+            if(totalNum == 0)
+            {
+                return;
+            }
+            int preX = tabAttributes.PaddingLeft.All.Value;
+            int preW = 0;
+            if (tabAttributes.IsNatureTextWidth == true)
+            {                
+                for (int i = 0; i < totalNum; i++)
+                {
+                    preW = itemList[i].GetNaturalSize().Width;
+                    itemList[i].Position2D.X = preX;
+                    itemList[i].Size2D.Width = preW;
+                    preX = itemList[i].Position2D.X + preW;
+                }
+            }
+            else
+            {
+                preW = (Size2D.Width - tabAttributes.PaddingLeft.All.Value - tabAttributes.PaddingRight.All.Value) / totalNum;
+                for (int i = 0; i < totalNum; i++)
+                {
+                    itemList[i].Position2D.X = preX;
+                    itemList[i].Size2D.Width = preW;
+                    preX = itemList[i].Position2D.X + preW;
+                }
+            }          
+        }
+
+        private void UpdateUnderLinePos()
+        {
+            if (underLine == null || tabAttributes.UnderLineAttributes == null || tabAttributes.UnderLineAttributes.Size2D == null)
+            {
+                return;
+            }
+
+            underLine.Size2D = new Size2D(itemList[curIndex].Size2D.Width, tabAttributes.UnderLineAttributes.Size2D.All.Height);
+            underLine.Position2D.X = itemList[curIndex].Position2D.X;
+
+            underLine.RaiseToTop();
+        }
+
+        private void UpdateSelectedItem(TabItem item)
+        {
+            if(item == null || curIndex == item.Index)
+            {
+                return;
+            }
+
+            ItemChangeEventArgs e = new ItemChangeEventArgs
+            {
+                PreviousIndex = curIndex,
+                CurrentIndex = item.Index
+            };
+            itemChangeHander?.Invoke(this, e);
+
+            itemList[curIndex].State = States.Normal;
+            itemList[curIndex].UpdateItemText(tabAttributes.TextAttributes);
+            curIndex = item.Index;
+            itemList[curIndex].State = States.Selected;
+            itemList[curIndex].UpdateItemText(tabAttributes.TextAttributes);
+
+            UpdateUnderLinePos();
+        }
+
+        private bool ItemTouchEvent(object source, TouchEventArgs e)
+        {
+            TabItem item = source as TabItem;
+            if(item == null)
+            {
+                return false;
+            }
+            PointStateType state = e.Touch.GetState(0);
+            if (state == PointStateType.Up)
+            {
+                UpdateSelectedItem(item);
+            }
+
+            return true;
+        }
+
+        public class TabItem : Control
+        {
+            private TextLabel text;
+            private int index;
+
+            public TabItem() : base()
+            {
+                text = new TextLabel()
+                {
+                    ParentOrigin = Tizen.NUI.ParentOrigin.Center,
+                    PivotPoint = Tizen.NUI.PivotPoint.Center,
+                    PositionUsesPivotPoint = true,
+                    WidthResizePolicy = ResizePolicyType.FillToParent,
+                    HeightResizePolicy = ResizePolicyType.FillToParent,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+                Add(text);
+            }
+
+            public string Text
+            {
+                get
+                {
+                    return text.Text;
+                }
+                set
+                {
+                    text.Text = value;
+                }
+            }
+
+            internal int Index
+            {
+                get
+                {
+                    return index;
+                }
+                set
+                {
+                    index = value;
+                }
+            }
+
+            internal TextLabel TextItem
+            {
+                get
+                {
+                    return text;
+                }
+            }
+
+            protected override void Dispose(DisposeTypes type)
+            {
+                if (disposed)
+                {
+                    return;
+                }
+
+                if (type == DisposeTypes.Explicit)
+                {
+                    if (text != null)
+                    {
+                        Remove(text);
+                        text.Dispose();
+                        text = null;
+                    }
+                }
+
+                base.Dispose(type);
+            }
+
+            protected override Attributes GetAttributes()
+            {
+                return null;
+            }
+
+            internal void UpdateItemText(TextAttributes attrs)
+            {
+                ApplyAttributes(text, attrs);
+            }
+        }
+
+        public class ItemChangeEventArgs : EventArgs
+        {
+            /// <summary> previous selected index of Tab </summary>
+            public int PreviousIndex;
+            /// <summary> current selected index of Tab </summary>
+            public int CurrentIndex;
+        }
+    }
+}
