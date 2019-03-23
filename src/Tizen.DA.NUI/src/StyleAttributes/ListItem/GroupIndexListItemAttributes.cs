f38e41006ad8f379581b23c6b070144cf3356ddf
gavin <lliang.zhang@samsung.com>
2019-03-23 12:01:25 +0800

    Refine ListItem with code backend style

--- src/Tizen.DA.NUI/src/StyleAttributes/ListItem/GroupIndexListItemAttributes.cs
+++ src/Tizen.DA.NUI/src/StyleAttributes/ListItem/GroupIndexListItemAttributes.cs
@@ -1,9 +1,72 @@
-﻿using Tizen.NUI.Xaml;
+﻿using Tizen.NUI;
+using Tizen.NUI.Controls;
+using Tizen.NUI.Xaml;
 
 [assembly: Tizen.NUI.Xaml.XamlResourceId("Tizen.FH.NUI.res.ListItem.GroupIndexListItemAttributes.xaml", "GroupIndexListItemAttributes.xaml", typeof(Tizen.FH.NUI.Controls.GroupIndexListItemAttributes))]
 namespace Tizen.FH.NUI.Controls
 {
     internal class GroupIndexListItemAttributes : AttributesContainer
     {
+        protected override Attributes GetAttributes()
+        {
+            if (Content != null)
+            {
+                return (Content as Attributes).Clone();
+            }
+            ListItemAttributes attributes = new ListItemAttributes
+            {
+                LeftSpace = 56,
+                RightSpace = 56,
+                SpaceBetweenRightItemAndText = 0,
+                BackgroundColor = new ColorSelector
+                {
+                    All = Utility.Hex2Color(0x282828, 0.05f),
+                },
+                SwitchStyle = "ListIndexSwitch",
+                StyleType = ListItem.StyleTypes.GroupIndex,
+                MainTextAttributes = new TextAttributes
+                {
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    PointSize = new FloatSelector
+                    {
+                        All = 30,
+                    },
+                    FontFamily = "SamsungOne 500",
+                    TextColor = new ColorSelector
+                    {
+                        All = new Color(0, 0, 0, 1),
+                    },
+                },
+                MainText2Attributes = new TextAttributes
+                {
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    PointSize = new FloatSelector
+                    {
+                        All = 24,
+                    },
+                    FontFamily = "SamsungOne 500C",
+                    TextColor = new ColorSelector
+                    {
+                        All = new Color(0, 0, 0, 1),
+                    }
+                },
+                RightItemRootViewAttributes = new ViewAttributes
+                {
+
+                },
+
+                RightIconAttributes = new ImageAttributes
+                {
+                    ResourceURL = new StringSelector
+                    {
+                        All = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_next_button.png"
+                    }
+                }
+            };
+
+            return attributes;
+        }
     }
 }
---
+++ src/Tizen.DA.NUI/src/StyleAttributes/Switch/ListIndexSwitchAttributes.cs
@@ -0,0 +1,42 @@
+﻿using Tizen.NUI.Xaml;
+using Tizen.NUI;
+using Tizen.NUI.Controls;
+
+[assembly: Tizen.NUI.Xaml.XamlResourceId("Tizen.FH.NUI.res.Switch.ListIndexSwitchAttributes.xaml", "ListIndexSwitchAttributes.xaml", typeof(Tizen.FH.NUI.Controls.ListIndexSwitchAttributes))]
+namespace Tizen.FH.NUI.Controls
+{
+    internal class ListIndexSwitchAttributes : AttributesContainer
+    {
+        protected override Attributes GetAttributes()
+        {
+            SwitchAttributes attributes = new SwitchAttributes
+            {
+                IsSelectable = true,
+                SwitchBackgroundImageAttributes = new ImageAttributes
+                {
+                    Size2D = new Size2D(72, 48),
+                    ResourceURL = new StringSelector
+                    {
+                        Normal = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_swich_bg_off.png",
+                        Selected = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_swich_bg_on.png",
+                        Disabled = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_swich_bg_off_dim.png",
+                        DisabledSelected = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_swich_bg_on_dim.png",
+                    },
+                },
+                SwitchHandlerImageAttributes = new ImageAttributes
+                {
+                    Size2D = new Size2D(48, 48),
+                    ResourceURL = new StringSelector
+                    {
+                        Normal = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_controller_swich.png",
+                        Selected = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_controller_swich.png",
+                        Disabled = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_controller_swich_dim.png",
+                        DisabledSelected = CommonResource.Instance.GetFHResourcePath() + "6. List/list_index_controller_swich_dim.png",
+                    },
+                },
+            };
+
+            return attributes;
+        }
+    }
+}
