546d21d6b16e5574c8cfdcfb69d9092162117ac5
lihua5.sun <lihua5.sun@samsung.com>
2019-03-28 09:38:44 +0800

    Refine sample

--- demo/Tizen.FH.NUI.Examples/Samples/PaginationSample.cs
+++ demo/Tizen.FH.NUI.Examples/Samples/PaginationSample.cs
@@ -5,6 +5,8 @@ namespace Tizen.FH.NUI.Samples
 {
     public class PaginationSample : IExample
     {
+        private SampleLayout root;
+
         private Pagination DAPagination1;
         private Pagination DAPagination2;
         private Pagination DAPagination3;
@@ -13,33 +15,34 @@ namespace Tizen.FH.NUI.Samples
 
         public void Activate()
         {
-            Window window = Window.Instance;
+            root = new SampleLayout(false);
+            root.HeaderText = "Pagination";
 
             ///////////////////////////////////////////////Create by Style//////////////////////////////////////////////////////////
             DAPagination1 = new Pagination("BasicPagination");
             DAPagination1.Name = "DAAppPagination1";
-            DAPagination1.Position2D = new Position2D(500, 450);
+            DAPagination1.Position2D = new Position2D(200, 50);
             DAPagination1.Size2D = new Size2D(400, 30);
             DAPagination1.BackgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.6f);
             DAPagination1.IndicatorCount = PAGE_COUNT;
             DAPagination1.SelectedIndex = 0;
             DAPagination1.Focusable = true;
-            window.Add(DAPagination1);
+            root.Add(DAPagination1);
 
             DAPagination2 = new Pagination("BasicPagination");
             DAPagination2.Name = "DAAppPagination2";
-            DAPagination2.Position2D = new Position2D(500, 500);
+            DAPagination2.Position2D = new Position2D(200, 150);
             DAPagination2.Size2D = new Size2D(230, 30);
             DAPagination2.BackgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.6f);
             DAPagination2.IndicatorCount = PAGE_COUNT;
             DAPagination2.SelectedIndex = 1;
             DAPagination2.Focusable = true;
-            window.Add(DAPagination2);
+            root.Add(DAPagination2);
 
             ///////////////////////////////////////////////Create by Properties//////////////////////////////////////////////////////////
             DAPagination3 = new Pagination();
             DAPagination3.Name = "DAAppPagination3";
-            DAPagination3.Position2D = new Position2D(500, 550);
+            DAPagination3.Position2D = new Position2D(200, 250);
             DAPagination3.Size2D = new Size2D(400, 30);
             DAPagination3.BackgroundColor = new Color(1.0f, 1.0f, 1.0f, 0.6f);
             DAPagination3.IndicatorSize = new Size2D(26, 26);
@@ -59,59 +62,58 @@ namespace Tizen.FH.NUI.Samples
             DAPagination3.IndicatorCount = PAGE_COUNT;
             DAPagination3.SelectedIndex = 0;
             DAPagination3.Focusable = true;
-            window.Add(DAPagination3);
-
-            window.KeyEvent += Window_KeyEvent;
+            root.Add(DAPagination3);
 
         }
 
-        private void Window_KeyEvent(object sender, Window.KeyEventArgs e)
-        {
-            if (e.Key.State == Key.StateType.Down)
-            {
-                if (e.Key.KeyPressedName == "Left")
-                {
-                    if (DAPagination1.SelectedIndex > 0)
-                    {
-                        DAPagination1.SelectedIndex = DAPagination1.SelectedIndex - 1;
-                    }
-                    if (DAPagination2.SelectedIndex > 0)
-                    {
-                        DAPagination2.SelectedIndex = DAPagination2.SelectedIndex - 1;
-                    }
-                    if (DAPagination3.SelectedIndex > 0)
-                    {
-                        DAPagination3.SelectedIndex = DAPagination3.SelectedIndex - 1;
-                    }
-                }
-                else if (e.Key.KeyPressedName == "Right")
-                {
-                    if (DAPagination1.SelectedIndex < DAPagination1.IndicatorCount - 1)
-                    {
-                        DAPagination1.SelectedIndex = DAPagination1.SelectedIndex + 1;
-                    }
-                    if (DAPagination2.SelectedIndex < DAPagination2.IndicatorCount - 1)
-                    {
-                        DAPagination2.SelectedIndex = DAPagination2.SelectedIndex + 1;
-                    }
-                    if (DAPagination3.SelectedIndex < DAPagination3.IndicatorCount - 1)
-                    {
-                        DAPagination3.SelectedIndex = DAPagination3.SelectedIndex + 1;
-                    }
-                }
-            }
-        }
+        //private void Window_KeyEvent(object sender, Window.KeyEventArgs e)
+        //{
+        //    if (e.Key.State == Key.StateType.Down)
+        //    {
+        //        if (e.Key.KeyPressedName == "Left")
+        //        {
+        //            if (DAPagination1.SelectedIndex > 0)
+        //            {
+        //                DAPagination1.SelectedIndex = DAPagination1.SelectedIndex - 1;
+        //            }
+        //            if (DAPagination2.SelectedIndex > 0)
+        //            {
+        //                DAPagination2.SelectedIndex = DAPagination2.SelectedIndex - 1;
+        //            }
+        //            if (DAPagination3.SelectedIndex > 0)
+        //            {
+        //                DAPagination3.SelectedIndex = DAPagination3.SelectedIndex - 1;
+        //            }
+        //        }
+        //        else if (e.Key.KeyPressedName == "Right")
+        //        {
+        //            if (DAPagination1.SelectedIndex < DAPagination1.IndicatorCount - 1)
+        //            {
+        //                DAPagination1.SelectedIndex = DAPagination1.SelectedIndex + 1;
+        //            }
+        //            if (DAPagination2.SelectedIndex < DAPagination2.IndicatorCount - 1)
+        //            {
+        //                DAPagination2.SelectedIndex = DAPagination2.SelectedIndex + 1;
+        //            }
+        //            if (DAPagination3.SelectedIndex < DAPagination3.IndicatorCount - 1)
+        //            {
+        //                DAPagination3.SelectedIndex = DAPagination3.SelectedIndex + 1;
+        //            }
+        //        }
+        //    }
+        //}
 
         public void Deactivate()
         {
-            Window window = Window.Instance;
-            window.Remove(DAPagination1);
-            window.Remove(DAPagination2);
-            window.Remove(DAPagination3);
+            root.Remove(DAPagination1);
+            root.Remove(DAPagination2);
+            root.Remove(DAPagination3);
 
             DAPagination1.Dispose();
             DAPagination2.Dispose();
             DAPagination3.Dispose();
+
+            root.Dispose();
         }
     }
 }
