674806e3097feffbe57e02f19f5feab66841fb19
gavin <lliang.zhang@samsung.com>
2019-03-28 14:14:56 +0800

    Update progress attributes

--- src/Tizen.NUI.CommonUI/Attributes/ProgressBarAttributes.cs
+++ src/Tizen.NUI.CommonUI/Attributes/ProgressBarAttributes.cs
@@ -4,323 +4,106 @@ namespace Tizen.NUI.CommonUI
 {
     public class ProgressBarAttributes : ViewAttributes
     {
-        public static readonly BindableProperty TrackImageAttributesProperty = BindableProperty.Create("TrackImageAttributes", typeof(ImageAttributes), typeof(ProgressBarAttributes), default(ImageAttributes), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.trackImageAttrs = (ImageAttributes)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.trackImageAttrs;
-        });
-
-        public static readonly BindableProperty ProgressImageAttributesProperty = BindableProperty.Create("ProgressImageAttributes", typeof(ImageAttributes), typeof(ProgressBarAttributes), default(ImageAttributes), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.progressImageAttrs = (ImageAttributes)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.progressImageAttrs;
-        });
-
-        public static readonly BindableProperty ProgressImageURLPrefixProperty = BindableProperty.Create("ProgressImageURLPrefix", typeof(StringSelector), typeof(ProgressBarAttributes), default(StringSelector), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.progressImageURLPrefix = (StringSelector)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.progressImageURLPrefix;
-        });
-
-        public static readonly BindableProperty BufferImageAttributesProperty = BindableProperty.Create("BufferImageAttributes", typeof(ImageAttributes), typeof(ProgressBarAttributes), default(ImageAttributes), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.bufferImageAttrs = (ImageAttributes)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.bufferImageAttrs;
-        });
-
-        public static readonly BindableProperty LoadingImageAttributesProperty = BindableProperty.Create("LoadingImageAttributes", typeof(ImageAttributes), typeof(ProgressBarAttributes), default(ImageAttributes), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.loadingImageAttrs = (ImageAttributes)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.loadingImageAttrs;
-        });
-
-        public static readonly BindableProperty ThumbSizeProperty = BindableProperty.Create("ThumbSize", typeof(Size), typeof(ProgressBarAttributes), default(Size), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.thumbSize = (Size)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.thumbSize;
-        });
-
-        public static readonly BindableProperty MaxValueProperty = BindableProperty.Create("MaxValue", typeof(uint), typeof(ProgressBarAttributes), default(uint), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.maxValue = (uint?)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.maxValue;
-        });
-
-        public static readonly BindableProperty MinValueProperty = BindableProperty.Create("MinValue", typeof(uint), typeof(ProgressBarAttributes), default(uint), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.minValue = (uint?)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.minValue;
-        });
-
-        public static readonly BindableProperty CurValueProperty = BindableProperty.Create("CurValue", typeof(uint), typeof(ProgressBarAttributes), default(uint), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.curValue = (uint?)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.curValue;
-        });
-
-        public static readonly BindableProperty DurationProperty = BindableProperty.Create("Duration", typeof(uint), typeof(ProgressBarAttributes), default(uint), propertyChanged: (bindable, oldValue, newValue) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            if (newValue != null)
-            {
-                attrs.duration = (uint?)newValue;
-            }
-        },
-        defaultValueCreator: (bindable) =>
-        {
-            var attrs = (ProgressBarAttributes)bindable;
-            return attrs.duration;
-        });
-
-        private StringSelector progressImageURLPrefix;
-        private Size thumbSize;
-        private uint? maxValue;
-        private uint? minValue;
-        private uint? curValue;
-        private uint? duration;
-        private ImageAttributes trackImageAttrs;
-        private ImageAttributes progressImageAttrs;
-        private ImageAttributes bufferImageAttrs;
-        private ImageAttributes loadingImageAttrs;
-        
         public ProgressBarAttributes() : base() { }
 
         public ProgressBarAttributes(ProgressBarAttributes attributes) : base(attributes)
         {
-            thumbSize = attributes.thumbSize;
-            maxValue = attributes.maxValue;
-            minValue = attributes.minValue;
-            curValue = attributes.curValue;
-            duration = attributes.duration;
+            ThumbSize = attributes.ThumbSize;
+            MaxValue = attributes.MaxValue;
+            MinValue = attributes.MinValue;
+            CurValue = attributes.CurValue;
+            Duration = attributes.Duration;
 
-            if (attributes.progressImageURLPrefix != null)
+            if (attributes.ProgressImageURLPrefix != null)
             {
-                progressImageURLPrefix = attributes.progressImageURLPrefix.Clone() as StringSelector;
+                ProgressImageURLPrefix = attributes.ProgressImageURLPrefix.Clone() as StringSelector;
             }
 
-            if (attributes.trackImageAttrs != null)
+            if (attributes.TrackImageAttributes != null)
             {
-                trackImageAttrs = attributes.trackImageAttrs.Clone() as ImageAttributes;
+                TrackImageAttributes = attributes.TrackImageAttributes.Clone() as ImageAttributes;
             }
 
-            if (attributes.progressImageAttrs != null)
+            if (attributes.ProgressImageAttributes != null)
             {
-                progressImageAttrs = attributes.progressImageAttrs.Clone() as ImageAttributes;
+                ProgressImageAttributes = attributes.ProgressImageAttributes.Clone() as ImageAttributes;
             }
 
-            if (attributes.bufferImageAttrs != null)
+            if (attributes.BufferImageAttributes != null)
             {
-                bufferImageAttrs = attributes.bufferImageAttrs.Clone() as ImageAttributes;
+                BufferImageAttributes = attributes.BufferImageAttributes.Clone() as ImageAttributes;
             }
 
-            if (attributes.loadingImageAttrs != null)
+            if (attributes.LoadingImageAttributes != null)
             {
-                loadingImageAttrs = attributes.loadingImageAttrs.Clone() as ImageAttributes;
+                LoadingImageAttributes = attributes.LoadingImageAttributes.Clone() as ImageAttributes;
             }
         }
 
         public ImageAttributes TrackImageAttributes
         {
-            get
-            {
-                return (ImageAttributes)GetValue(TrackImageAttributesProperty);
-            }
-            set
-            {
-                SetValue(TrackImageAttributesProperty, value);
-            }
+            get;
+            set;
         }
 
         public ImageAttributes ProgressImageAttributes
         {
-            get
-            {
-                return (ImageAttributes)GetValue(ProgressImageAttributesProperty);
-            }
-            set
-            {
-                SetValue(ProgressImageAttributesProperty, value);
-            }
+            get;
+            set;
         }
 
         public ImageAttributes BufferImageAttributes
         {
-            get
-            {
-                return (ImageAttributes)GetValue(BufferImageAttributesProperty);
-            }
-            set
-            {
-                SetValue(BufferImageAttributesProperty, value);
-            }
+            get;
+            set;
         }
 
         public ImageAttributes LoadingImageAttributes
         {
-            get
-            {
-                return (ImageAttributes)GetValue(LoadingImageAttributesProperty);
-            }
-            set
-            {
-                SetValue(LoadingImageAttributesProperty, value);
-            }
+            get;
+            set;
         }
 
         public StringSelector ProgressImageURLPrefix
         {
-            get
-            {
-                return (StringSelector)GetValue(ProgressImageURLPrefixProperty);
-            }
-            set
-            {
-                SetValue(ProgressImageURLPrefixProperty, value);
-            }
+            get;
+            set;
         }
 
         public Progress.DirectionType Direction
         {
-            get
-            {
-                return (Progress.DirectionType)GetValue(DurationProperty);
-            }
-            set
-            {
-                SetValue(DurationProperty, value);
-            }
+            get;
+            set;
         }
 
         public Size ThumbSize
         {
-            get
-            {
-                return (Size)GetValue(ThumbSizeProperty);
-            }
-            set
-            {
-                SetValue(ThumbSizeProperty, value);
-            }
+            get;
+            set;
         }
 
         public uint? MaxValue
         {
-            get
-            {
-                return (uint?)GetValue(MaxValueProperty);
-            }
-            set
-            {
-                SetValue(MaxValueProperty, value);
-            }
+            get;
+            set;
         }
 
         public uint? MinValue
         {
-            get
-            {
-                return (uint?)GetValue(MinValueProperty);
-            }
-            set
-            {
-                SetValue(MinValueProperty, value);
-            }
+            get;
+            set;
         }
 
         public uint? CurValue
         {
-            get
-            {
-                return (uint?)GetValue(CurValueProperty);
-            }
-            set
-            {
-                SetValue(CurValueProperty, value);
-            }
+            get;
+            set;
         }
 
         public uint? Duration
         {
-            get
-            {
-                return (uint?)GetValue(DurationProperty);
-            }
-            set
-            {
-                SetValue(DurationProperty, value);
-            }
+            get;
+            set;
         }
 
         public override Attributes Clone()
