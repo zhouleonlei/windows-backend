cc0db93a1841823ca8f7241757ebf4f0b4e007af
jiawei.xiang <jiawei.xiang@samsung.com>
2019-04-02 09:53:46 +0800

    [Picker] Use SampleLayout to write sample.

--- demo/Tizen.FH.NUI.Examples/Samples/DatePickerSample.cs
+++ demo/Tizen.FH.NUI.Examples/Samples/DatePickerSample.cs
@@ -11,66 +11,67 @@ namespace Tizen.FH.NUI.Samples
 {
     public class DatePickerSample : IExample
     {
-        private static string[] buttonStyles = new string[]
-        {
-            "UtilityPopupButton",
-            "FamilyPopupButton",
-            "FoodPopupButton",
-            "KitchenPopupButton",
-        };
-        
-        private View root;
-        private Popup popup;
-        private Picker datePicker;
-        private int index = 0;
+        private SampleLayout root;
+        private Popup popup = null;
+        private Picker datePicker = null;
+
         public void Activate()
         {
             Window window = Window.Instance;
-            
-            root = new View()
-            {
-                Size2D = new Size2D(1920, 1080),
-                BackgroundColor = Color.Cyan,
-            };
 
+            root = new SampleLayout();
+            root.HeaderText = "Date Picker";
 
+            CreateDatePicker();
+        }
+
+        private void CreateDatePicker()
+        {
             popup = new Popup("Popup");
             popup.Size2D = new Size2D(1032, 982);
+            popup.Position2D = new Position2D(24, 50);
             popup.TitleText = "Set Date";
             popup.ButtonCount = 2;
-            popup.SetButtonText(0, "Yes");
-            popup.SetButtonText(1, "Exit");
+            popup.SetButtonText(0, "Cancel");
+            popup.SetButtonText(1, "OK");
             popup.PopupButtonClickedEvent += PopupButtonClickedEvent;
             root.Add(popup);
-
+ 
             datePicker = new Picker("DAPicker");
             datePicker.Size2D = new Size2D(1032, 724);
             datePicker.Position2D = new Position2D(0, 0);
             datePicker.CurDate = new DateTime(2012, 12, 12);
             popup.ContentView.Add(datePicker);
-            
-            window.Add(root);
-            Console.WriteLine("month" + DateTime.Now.ToString("MMMM", new CultureInfo("en-us")));
         }
 
         private void PopupButtonClickedEvent(object sender, Popup.ButtonClickEventArgs e)
         {
                      
         }
-        public void Deactivate()
+        
+        private void DestoryDatePicker()
         {
-            if (datePicker != null)
+            if (popup != null)
             {
-                root.Remove(datePicker);
-                datePicker.Dispose();
-                datePicker = null;
-            }
+                if (datePicker != null)
+                {
+                    popup.ContentView.Remove(datePicker);
+                    datePicker.Dispose();
+                    datePicker = null;
+                }
 
+                root.Remove(popup);
+                popup.Dispose();
+                popup = null;
+            }
+        }
+
+        public void Deactivate()
+        {         
             if (root != null)
             {
-                Window.Instance.Remove(root);
+                DestoryDatePicker();
                 root.Dispose();
-                root = null;
             }
         }
     }
--- demo/Tizen.FH.NUI.Examples/Samples/TimePickerSample.cs
+++ demo/Tizen.FH.NUI.Examples/Samples/TimePickerSample.cs
@@ -7,154 +7,153 @@ namespace Tizen.FH.NUI.Samples
 {
     public class TimePickerSample : IExample
     {
-        private static string[] buttonStyles = new string[]
-        {
-            "UtilityPopupButton",
-            "FamilyPopupButton",
-            "FoodPopupButton",
-            "KitchenPopupButton",
-        };
+        private SampleLayout root;
+        private Popup popup = null;
+        private TimePicker timePicker = null;
+        private Button[] button = new Button[3];
+        private int num = 3;
 
-        private View[] root;
-        private View FHView;
-        private Popup popup;
-        private TimePicker timePicker;
-        
-        private View FHViewAMPM;
-        private Popup popupAMPM;
-        private TimePicker timePickerAMPM;
-        
-        private View FHViewRepeat;
-        private Popup popupRepeat;
-        private TimePicker timePickerRepeat;
-        private int index = 0;
+        private static string[] mode = new string[]
+        {
+            "TimePicker",
+            "TimePicker with AMPM",
+            "TimePicker with Repeat",
+        };
         public void Activate()
         {
             Window window = Window.Instance;
 
-            root = new View[3];
+            root = new SampleLayout();
+            root.HeaderText = "Time Picker";
 
-            FHView = new View()
+            CreateBaseTimePicker();
+            
+            for (int i = 0; i < num; i++)
             {
-                Size2D = new Size2D(1920, 1080),
-                BackgroundColor = Color.White,
-            };
-            root[0] = FHView;
+                button[i] = new Button("ServiceButton");
+                button[i].Size2D = new Size2D(240, 80);
+                button[i].Position2D = new Position2D(160 + i * 260, 1350);
+                button[i].Text = mode[i];
+                button[i].PointSize = 11;
+                button[i].ClickEvent += ButtonClickEvent;
+                root.Add(button[i]);
+            }
+        }
 
+        private void CreateBaseTimePicker()
+        {
+            DestoryTimePicker();
             popup = new Popup("Popup");
             popup.Size2D = new Size2D(1032, 776);
+            popup.Position2D = new Position2D(24, 50);
             popup.TitleText = "Timer 01";
             popup.ButtonCount = 2;
             popup.SetButtonText(0, "Cancel");
             popup.SetButtonText(1, "OK");
             popup.PopupButtonClickedEvent += PopupButtonClickedEvent;
-            FHView.Add(popup);
+            root.Add(popup);
  
             timePicker = new TimePicker("DATimePicker");
             timePicker.Size2D = new Size2D(1032, 524);
             timePicker.Position2D = new Position2D(0, 0);
             popup.ContentView.Add(timePicker);
-            window.Add(FHView);
-            
-
-            FHViewAMPM = new View()
-            {
-                Size2D = new Size2D(1920, 1080),
-                BackgroundColor = Color.Cyan,
-            };
-            root[1] = FHViewAMPM;
-            
-            popupAMPM = new Popup("Popup");
-            popupAMPM.Size2D = new Size2D(1032, 776);
-            popupAMPM.TitleText = "Timer 02";
-            popupAMPM.ButtonCount = 2;
-            popupAMPM.SetButtonText(0, "Cancel");
-            popupAMPM.SetButtonText(1, "OK");
-            popupAMPM.PopupButtonClickedEvent += PopupButtonClickedEvent;
-            FHViewAMPM.Add(popupAMPM);
- 
-            timePickerAMPM = new TimePicker("DATimePickerAMPM");
-            timePickerAMPM.Size2D = new Size2D(1032, 524);
-            timePickerAMPM.Position2D = new Position2D(0, 0);
-            popupAMPM.ContentView.Add(timePickerAMPM);
-            window.Add(FHViewAMPM);
-
-            FHViewRepeat = new View()
-            {
-                Size2D = new Size2D(1920, 1080),
-                BackgroundColor = Color.Cyan,
-            };
-            root[2] = FHViewRepeat;
-
-            popupRepeat = new Popup("Popup");
-            popupRepeat.Size2D = new Size2D(1032, 1064);
-            popupRepeat.TitleText = "Time Setting";
-            popupRepeat.ButtonCount = 2;
-            popupRepeat.SetButtonText(0, "Cancel");
-            popupRepeat.SetButtonText(1, "OK");
-            popupRepeat.PopupButtonClickedEvent += PopupButtonClickedEvent;
-            FHViewRepeat.Add(popupRepeat);
- 
-            timePickerRepeat = new TimePicker("DATimePickerRepeat");
-            timePickerRepeat.Size2D = new Size2D(1032, 812);
-            timePickerRepeat.Position2D = new Position2D(0, 0);
-            popupRepeat.ContentView.Add(timePickerRepeat);
-            window.Add(FHViewRepeat);
-            
-            index = 0;
-            root[0].Show();
-            root[1].Hide();
-            root[2].Hide();
         }
 
+        private void CreateAMPMTimePicker()
+        {
+            DestoryTimePicker();
+            popup = new Popup("Popup");
+            popup.Size2D = new Size2D(1032, 776);
+            popup.Position2D = new Position2D(24, 50);
+            popup.TitleText = "Timer 02";
+            popup.ButtonCount = 2;
+            popup.SetButtonText(0, "Cancel");
+            popup.SetButtonText(1, "OK");
+            popup.PopupButtonClickedEvent += PopupButtonClickedEvent;
+            root.Add(popup);
+ 
+            timePicker = new TimePicker("DATimePickerAMPM");
+            timePicker.Size2D = new Size2D(1032, 524);
+            timePicker.Position2D = new Position2D(0, 0);
+            popup.ContentView.Add(timePicker);
+        }
+
+        private void CreateRepeatTimePicker()
+        {
+            DestoryTimePicker();
+            popup = new Popup("Popup");
+            popup.Size2D = new Size2D(1032, 1064);
+            popup.Position2D = new Position2D(24, 50);
+            popup.TitleText = "Timer 02";
+            popup.ButtonCount = 2;
+            popup.SetButtonText(0, "Cancel");
+            popup.SetButtonText(1, "OK");
+            popup.PopupButtonClickedEvent += PopupButtonClickedEvent;
+            root.Add(popup);
+ 
+            timePicker = new TimePicker("DATimePickerRepeat");
+            timePicker.Size2D = new Size2D(1032, 812);
+            timePicker.Position2D = new Position2D(0, 0);
+            popup.ContentView.Add(timePicker);
+        }
+        
+        
         private void PopupButtonClickedEvent(object sender, Popup.ButtonClickEventArgs e)
         {
-            if (e.ButtonIndex == 1)
+            
+        }
+
+        private void ButtonClickEvent(object sender, Button.ClickEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (button[0] == btn)
             {
-                root[index].Hide();
-                index = (index+1)%3;
-                root[index].Show();            
+                CreateBaseTimePicker();
+            }
+            else if (button[1] == btn)
+            {
+                CreateAMPMTimePicker();
+            }
+            else if (button[2] == btn)
+            {
+                CreateRepeatTimePicker();
             }
         }
-        public void Deactivate()
+
+        private void DestoryTimePicker()
         {
-            if (FHView != null)
+            if (popup != null)
             {
                 if (timePicker != null)
                 {
-                    FHView.Remove(timePicker);
+                    popup.ContentView.Remove(timePicker);
                     timePicker.Dispose();
                     timePicker = null;
                 }
-                Window.Instance.Remove(FHView);
-                FHView.Dispose();
-                FHView = null;
-            }
 
-            if (FHViewAMPM != null)
-            {
-                if (timePickerAMPM != null)
-                {
-                    FHViewAMPM.Remove(timePickerAMPM);
-                    timePickerAMPM.Dispose();
-                    timePickerAMPM = null;
-                }
-                Window.Instance.Remove(FHViewAMPM);
-                FHViewAMPM.Dispose();
-                FHViewAMPM = null;
+                root.Remove(popup);
+                popup.Dispose();
+                popup = null;
             }
+        }
 
-            if (FHViewRepeat != null)
+        public void Deactivate()
+        {         
+            if (root != null)
             {
-                if (timePickerRepeat != null)
+                DestoryTimePicker();
+
+                for (int i = 0; i < num; i++)
                 {
-                    FHViewRepeat.Remove(timePickerRepeat);
-                    timePickerRepeat.Dispose();
-                    timePickerRepeat = null;
+                    if (button[i] != null)
+                    {
+                        root.Remove(button[i]);
+                        button[i].Dispose();
+                        button[i] = null;
+                    }
                 }
-                Window.Instance.Remove(FHViewRepeat);
-                FHViewRepeat.Dispose();
-                FHViewRepeat = null;
+
+                root.Dispose();
             }
         }
     }
