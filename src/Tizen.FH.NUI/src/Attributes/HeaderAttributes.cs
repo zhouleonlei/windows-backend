2820f3f75c0f00e23b2b2a5d53ee9db4f66b2347
xd123.liu <xd123.liu@samsung.com>
2019-04-04 16:22:53 +0800

    Add comment for HeaderAttributes and NaviFrameAttributes

--- src/Tizen.FH.NUI/src/Attributes/HeaderAttributes.cs
+++ src/Tizen.FH.NUI/src/Attributes/HeaderAttributes.cs
@@ -1,15 +1,44 @@
-﻿using System.ComponentModel;
+﻿/*
+ * Copyright(c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System.ComponentModel;
 using Tizen.NUI.CommonUI;
 
 namespace Tizen.FH.NUI.Controls
 {
-    /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
+    /// <summary>
+    /// HeaderAttributes is a class which saves Header's ux data.
+    /// </summary>
+    /// <since_tizen> 5.5 </since_tizen>    
+	/// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class HeaderAttributes : ViewAttributes
     {
+        /// <summary>
+        /// Creates a new instance of a HeaderAttributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public HeaderAttributes() : base() { }
+        /// <summary>
+        /// Creates a new instance of a HeaderAttributes with attributes.
+        /// </summary>
+        /// <param name="attributes">Create HeaderAttributes by attributes customized by user.</param>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public HeaderAttributes(HeaderAttributes attributes) : base(attributes)
@@ -24,6 +53,10 @@ namespace Tizen.FH.NUI.Controls
             }
         }
 
+        /// <summary>
+        /// Header text's attributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public TextAttributes TextAttributes
@@ -31,6 +64,10 @@ namespace Tizen.FH.NUI.Controls
             get;
             set;
         }
+        /// <summary>
+        /// Header line's attributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ViewAttributes LineAttributes
@@ -39,6 +76,10 @@ namespace Tizen.FH.NUI.Controls
             set;
         }
 
+        /// <summary>
+        /// Attributes's clone function.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override Attributes Clone()
--- src/Tizen.FH.NUI/src/Attributes/NaviFrameAttributes.cs
+++ src/Tizen.FH.NUI/src/Attributes/NaviFrameAttributes.cs
@@ -1,16 +1,44 @@
-﻿
+﻿/*
+ * Copyright(c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 using System.ComponentModel;
 using Tizen.NUI.CommonUI;
 
 namespace Tizen.FH.NUI.Controls
 {
+    /// <summary>
+    /// NaviFrameAttributes is a class which saves Naviframe's ux data.
+    /// </summary>
     /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class NaviFrameAttributes : ViewAttributes
     {
+        /// <summary>
+        /// Creates a new instance of a NaviFrameAttributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public NaviFrameAttributes() : base() { }
+
+        /// <summary>
+        /// Creates a new instance of a NaviFrameAttributes with attributes.
+        /// </summary>
+        /// <param name="attributes">Create NaviFrameAttributes by attributes customized by user.</param>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public NaviFrameAttributes(NaviFrameAttributes attributes) : base(attributes)
@@ -25,6 +53,11 @@ namespace Tizen.FH.NUI.Controls
             }
 
         }
+
+        /// <summary>
+        /// NaviFrame header's attributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ViewAttributes NaviHeaderAttributes
@@ -32,6 +65,11 @@ namespace Tizen.FH.NUI.Controls
             get;
             set;
         }
+
+        /// <summary>
+        /// NaviFrame content's attributes.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ViewAttributes ContentAttributes
@@ -39,6 +77,11 @@ namespace Tizen.FH.NUI.Controls
             get;
             set;
         }
+
+        /// <summary>
+        /// Attributes's clone function.
+        /// </summary>
+        /// <since_tizen> 5.5 </since_tizen>
         /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override Attributes Clone()
